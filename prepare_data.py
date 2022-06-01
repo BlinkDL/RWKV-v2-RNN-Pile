e419c6e9785fc7aefdff5391cc919fd342ff6600
Pathos14489 <shinigamiking14489@gmail.com>
2022-06-01 10:23:51 -0700

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -3,12 +3,13 @@
 ########################################################################################################
 
 import numpy as np
+from torch import le
 
 from transformers import PreTrainedTokenizerFast
 tokenizer = PreTrainedTokenizerFast(tokenizer_file='20B_tokenizer.json')
 
-input_file = 'training.txt'
-output_file = 'train.npy'
+input_file = 'big.txt'
+output_file = 'train2.npy'
 
 TASK = 'tokenize' # tokenize verify
 
@@ -19,7 +20,22 @@ if TASK == 'tokenize':
     data_raw = open(input_file).read()
     print(f'Raw length = {len(data_raw)}')
 
-    data_code = tokenizer.encode(data_raw)
+    # if len(data_raw) > 1000000 split into chunks -- Lets you tokenize a large file without running out of memory
+    chunks = []
+    if len(data_raw) > 1000000:
+        print('Splitting into chunks')
+        chunks = [data_raw[i:i+1000000] for i in range(0, len(data_raw), 1000000)]
+    else:
+        print('No need to split')
+        chunks = [data_raw]
+    
+
+    data_code = []
+    print(f'Total chunks = {len(chunks)}')
+    for chunk in chunks:
+        print(f'Tokenizing chunk {len(chunks)-chunks.index(chunk)}')
+        data_code = data_code + tokenizer.encode(chunk)
+        print(len(data_code))
     print(f'Tokenized length = {len(data_code)}')
 
     out = np.array(data_code, dtype='uint16')
