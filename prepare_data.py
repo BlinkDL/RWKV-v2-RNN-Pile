333a0a062f753a214cd00b91d777a608d5cff08d
Pathos14489 <shinigamiking14489@gmail.com>
2022-06-01 20:54:44 -0700

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -3,42 +3,99 @@
 ########################################################################################################
 
 import numpy as np
+from torch import le
+
+import os
 
 from transformers import PreTrainedTokenizerFast
+from threading import Thread
 tokenizer = PreTrainedTokenizerFast(tokenizer_file='20B_tokenizer.json')
 
-input_file = 'training.txt'
-output_file = 'train.npy'
+input_dir = './training/'
+os.makedirs(input_dir, exist_ok=True)
+output_file = 'fimfic.npy'
+
+threadCount = 1 # number of threads to use -- unfortunately, 1 seems to be the fastest on my setup. Maybe it's bottlenecked by the SSD, or the CPU? none of these threads seem to be maxing usage, so give it a shot.
+reuse_old_chunks = True
+chunksDir = "./training_chunks/"
+os.makedirs(chunksDir, exist_ok=True)
+
+keep_buffer = True
+
 
 TASK = 'tokenize' # tokenize verify
 
+files = os.listdir(input_dir)
+files = [f for f in files if f.endswith('.txt')] # id-chapterid.txt
+# sort first by id, then by chapterid
+files = sorted(files, key=lambda x: (int(x.split('-')[0]), int(x.split('-')[1].split('.')[0])))
+
+
+data_code = []
 if TASK == 'tokenize':
-
-    print(f'Tokenizing {input_file} (VERY slow. please wait)')
-
-    data_raw = open(input_file).read()
-    print(f'Raw length = {len(data_raw)}')
-
-    # if len(data_raw) > 1000000 split into chunks -- Lets you tokenize a large file without running out of memory
-    chunks = []
-    if len(data_raw) > 1000000:
-        print('Splitting into chunks')
-        chunks = [data_raw[i:i+1000000] for i in range(0, len(data_raw), 1000000)]
-    else:
-        print('No need to split')
-        chunks = [data_raw]
-    
-
-    data_code = []
-    print(f'Total chunks = {len(chunks)}')
-    for chunk in chunks:
-        print(f'Tokenizing chunk {len(chunks)-chunks.index(chunk)}')
+    rawChunks = []
+    def parseChunk(chunk,buf,i):
+        global rawChunks
+        global data_code
+        if(not buf):
+            chunk = open(chunksDir + chunk).read()
+        print(f'{i}:Tokenizing chunk {len(rawChunks)}/{len(os.listdir(chunksDir))}')
         data_code = data_code + tokenizer.encode(chunk)
         print(len(data_code))
-    print(f'Tokenized length = {len(data_code)}')
+        print(f'{i}:Tokenized length = {len(data_code)}')
+    chunkSize = 1000000
+    if reuse_old_chunks:
+        print("Loading old chunk data...")
+    else:
+        print("Loading raw text data...")
+        # clean up old chunks
+        for chunk in os.listdir(chunksDir):
+            os.remove(chunksDir + chunk)    
+        for input_file in files:
+            print(f'Loading {input_file}...')
+            chunk = open(input_dir + input_file).read()
+            if len(rawChunks) == 0:
+                print("First chunk")
+                rawChunks.append(chunk)
+            if (len(rawChunks[-1]) + len(chunk)) < chunkSize:
+                rawChunks[-1] = f"{rawChunks[-1]}\n{chunk}"
+                # print(len(rawChunks[-1]))
+            else:
+                print(f"{len(rawChunks)} chunks")
+                rawChunks.append(chunk)
+                with open(chunksDir + str(len(rawChunks)) + '.txt', 'w') as f:
+                    f.write(chunk)
+    rawChunks = os.listdir(chunksDir) # clearing the actual chunks from ram and instead rereads from disk to save on memory usage during tokenization -- TODO: make this more efficient and earlier in the process so the memory usage doesn't spike during text chunking
+    print(f'Total chunks = {len(rawChunks)}')
+    buffer = []
+    if keep_buffer:
+        bufferSize = threadCount * 2
+        while len(buffer) < bufferSize:
+            raw_chunk = rawChunks.pop()
+            chunk_data = open(chunksDir + raw_chunk).read()
+            buffer.append(chunk_data)
 
-    out = np.array(data_code, dtype='uint16')
-    np.save(output_file, out, allow_pickle=False)
+    while len(rawChunks) > 0:
+        threads = []
+        for i in range(min(len(rawChunks), threadCount)):
+            if len(rawChunks) == 0:
+                break
+            print(f'Starting tokenizing thread {i}')
+            if keep_buffer:
+                BUF = buffer.pop()
+                thread = Thread(target=parseChunk, args=(BUF,True,i))
+            else:
+                CHNK = rawChunks.pop()
+                thread = Thread(target=parseChunk, args=(CHNK,False,i))
+            thread.start()
+            threads.append(thread)
+        for thread in threads:
+            thread.join()
+            if keep_buffer:
+                raw_chunk = rawChunks.pop()
+                chunk_data = open(chunksDir + raw_chunk).read()
+                buffer.append(chunk_data)
+
 
 elif TASK == 'verify':
 
@@ -48,3 +105,6 @@ elif TASK == 'verify':
     print(tokenizer.decode(test[:100]))
     print('\n\n')
     print(tokenizer.decode(test[-100:]))
+        
+out = np.array(data_code, dtype='uint16')
+np.save(output_file, out, allow_pickle=False)
